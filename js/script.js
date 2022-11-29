7d5869e0d33eec595d7670ce439f078b410467f0
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-11-29 17:47:55 +0100

    modifica classi in css

--- js/script.js
+++ js/script.js
@@ -3,34 +3,34 @@ const container = document.querySelector('.container');
 // CICLO FOR
 
 for(let i=1; i<=100; i++){
-    let element;
+    let box;
 
     // multipli di 3 e 5
 
-    if(i % 3 == 0 && i % 5 == 0){
+    if(i%3 == 0 && i%5 == 0){
         console.log('FizzBuzz');
-        element = `div class="box box-red">FizzBuzz</div>`;
+        box = `div class="box box-red">FizzBuzz</div>`;
     }
 
     // multipli di 3
 
-    else if(i % 3 == 0){
+    else if(i%3 == 0){
         console.log('Fizz');
-        element = `div class="box box-beige">Fizz</div>`;
+        box = `div class="box box-beige">Fizz</div>`;
     }
 
     // multipli di 5
 
-    else if(i % 5 == 0){
+    else if(i%5 == 0){
         console.log('Buzz');
-        element = `div class="box box-yellow">Buzz</div>`;
+        box = `div class="box box-yellow">Buzz</div>`;
     }
 
     else{
         console.log(i);
-        element = `div class="box">${i}</div>`;
+        box = `div class="box">${i}</div>`;
     }
 
-    container.innerHTML += element;
+    container.innerHTML += box;
 
 }
