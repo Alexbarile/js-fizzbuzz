5be482907ffd19d0d97f4da43dc6b07d09e6734b
“Alexbarile” <alexbarile93fg@yahoo.it>
2022-11-29 18:31:05 +0100

    aggiunto h1 in html e modifica css per stilizzare

--- js/script.js
+++ js/script.js
@@ -9,26 +9,26 @@ for(let i=1; i<=100; i++){
 
     if(i%3 == 0 && i%5 == 0){
         console.log('FizzBuzz');
-        box = `div class="box box-red">FizzBuzz</div>`;
+        box = `<div class="box box-red">FizzBuzz</div>`;
     }
 
     // multipli di 3
 
     else if(i%3 == 0){
         console.log('Fizz');
-        box = `div class="box box-beige">Fizz</div>`;
+        box = `<div class="box box-beige">Fizz</div>`;
     }
 
     // multipli di 5
 
     else if(i%5 == 0){
         console.log('Buzz');
-        box = `div class="box box-yellow">Buzz</div>`;
+        box = `<div class="box box-yellow">Buzz</div>`;
     }
 
     else{
         console.log(i);
-        box = `div class="box">${i}</div>`;
+        box = `<div class="box">${i}</div>`;
     }
 
     container.innerHTML += box;
